683fd2ab8982e9d7dfb4f754b9a46030fdb5d8b9
Ikaro <Ikaro>
2014-12-12 02:51:41 -0300

    Adicionado  botão voltar.

---
+++ src/controller/frameworkGerenciaTela/ControlledScreenWithObject.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller.frameworkGerenciaTela;
+
+import model.Venda;
+
+/**
+ *
+ * @author comum
+ */
+public interface ControlledScreenWithObject {
+     public void setScreenParentwithObject(ScreensController screenPage, Venda venda);
+}
